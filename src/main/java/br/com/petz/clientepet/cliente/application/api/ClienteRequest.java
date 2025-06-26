70ee9ea92ab9c37c49d2e9a62c45f8d422ec4fd2
phellipe <phelliperrp@gmail.com>
2025-06-26 13:53:09 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
@@ -1,4 +1,32 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import br.com.petz.clientepet.cliente.domain.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+@Value
 public class ClienteRequest {
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String telefone;
+    private Sexo sexo;
+
+    @NotNull
+    private LocalDate dataNascimento;
+    @CPF
+    private String cpf;
+
+    @NotNull
+    private Boolean aceitaTermos;
 }
--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
@@ -1,4 +1,10 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
 public class ClienteResponse {
+    private UUID idCliente;
 }

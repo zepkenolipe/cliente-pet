a5d38190016feec558dba24f03e8d3a559a1739a
phellipe <phelliperrp@gmail.com>
2025-06-26 13:14:21 -0300

    implementa controller e fluxo para service

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -0,0 +1,22 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import br.com.petz.clientepet.cliente.application.service.ClienteService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class ClienteController implements ClienteAPI {
+    private final ClienteService clienteService;
+
+    @Override
+    public ClienteResponse postCliente(ClienteRequest clienteRequest) {
+        log.info("[inicia] ClienteController - postCliente");
+        ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequest);
+        log.info("[finaliza] ClienteController - postCliente");
+        return clienteCriado;
+
+    }
+}
---
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteService.java
@@ -0,0 +1,10 @@
+package br.com.petz.clientepet.cliente.application.service;
+
+import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
+import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ClienteService {
+    ClienteResponse criaCliente(ClienteRequest clienteRequest);
+}

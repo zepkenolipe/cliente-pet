4a8f0c4d5e81fb2d74b5f8fb1192111d18002e57
phellipe <phelliperrp@gmail.com>
2025-07-07 17:46:58 -0300

    implementa get todos

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
@@ -1,10 +1,13 @@
 package br.com.petz.clientepet.cliente.application.api;
 
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.Value;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class ClienteListResponse {
     private UUID idCliente;
     private String nome;
@@ -13,6 +16,16 @@ public class ClienteListResponse {
     private String email;
 
     public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-        return null;
+        return clientes.stream()
+                .map(ClienteListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    private ClienteListResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.email = cliente.getEmail();
+        this.celular = cliente.getCelular();
+        this.cpf = cliente.getCpf();
+        this.nome = cliente.getNome();
     }
 }
